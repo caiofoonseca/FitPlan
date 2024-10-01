588f7897f2d0552a9b29f5047b6bfbbab0c59780
Caio Fonseca <cmaf@cesar.school>
2024-10-01 14:35:44 -0300

    update

--- FitPlan/settings.py
+++ FitPlan/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-6)o((ybe3rdi0ch587%2@gso3s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost,fitplan.com,www.fitplan.com').split(',')
 
 # Application definition
 
